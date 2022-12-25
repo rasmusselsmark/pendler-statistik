49e3d1e0d417c105d287d9cf1d631c4f1acf8904
Rasmus Selsmark <rasmus@unity3d.com>
2022-12-25 20:15:32 +0100

    Pass station id as argument

--- TrainStats.Service/Program.cs
+++ TrainStats.Service/Program.cs
@@ -3,10 +3,33 @@ using TrainStats.Service.Services;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => AboutService.About());
+// todo: remove when out of development
+app.UseDeveloperExceptionPage();
+app.MapGet("/error", () => { throw new System.Exception("Error"); });
+
+app.MapGet("/", () => "Se https://github.com/rasmusselsmark/pendler-statistik for mere information");
+app.MapGet("/about", () => AboutService.About());
 app.MapGet("/install", () => DatabaseService.Install());
-app.MapGet("/trains", async () => await TrainService.GetTrainsAsync("HH"));
-app.MapGet("/fetch", async () => await TrainService.FetchAndStoreAsync("HH"));
-app.MapGet("/query", () => DatabaseService.Query("HH"));
+
+app.MapGet(
+    "/trains/{stationId:alpha}",
+    async (string stationId) =>
+    {
+        return await TrainService.GetTrainsAsync(stationId);
+    });
+
+app.MapGet(
+    "/fetch/{stationId:alpha}",
+    async (string stationId) =>
+    {
+        return await TrainService.FetchAndStoreAsync(stationId);
+    });
+
+app.MapGet(
+    "/query/{stationId:alpha}",
+    async (string stationId) =>
+    {
+        return await DatabaseService.Query(stationId);
+    });
 
 app.Run();
